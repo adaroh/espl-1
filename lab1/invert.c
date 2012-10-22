d1301b68f944607cefdef4993131a0c922a8f466
Yariv Zur <yarivz@post.bgu.ac.il>
2012-10-22 14:37:56 -0700

    added lab1

---
+++ lab1/invert.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+/* invert arguments */
+int main(int argc, char **argv) {
+  int i;
+ 
+  for(i=argc-1; i!=0; --i) {
+    printf("%s", argv[i]);
+ 
+    if(i!=0)
+    printf(" ");
+  }
+  printf("'.\n");
+ 
+  return 0;
+}
