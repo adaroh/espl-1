b60c6f9918604e1baeae1866cde6bfb13b482f85
Yariv Zur <yarivz@post.bgu.ac.il>
2012-10-24 20:06:51 +0200

    bla3

--- barcode/encbc.c
+++ barcode/encbc.c
@@ -3,35 +3,24 @@
 #include "bclib.h"
 
 int main (int argc, char **argv){
-  int i,j;
-  int k=0;
-  int *bits;
+  int i,j,k,m,l;
   
   int barcode[BC_WIDTH][BC_NBITS]={0};
   for(i=1; i<argc; i++){
-    for(j=0; argv[i][j]!=0; j++){
-      bc_char2bits(argv[i][j],barcode[k]);
-      k++;
+      if(i>=2) {bc_char2bits(' ',barcode[k]);k++;}
+      for(j=0; argv[i][j]!=0; j++){
+	bc_char2bits(argv[i][j],barcode[k]);
+	k++;
+      }
     }
-  k++;
-  }
-  bc_print(barcode);
-  
-  
-return 0;
+    for(l=0;l<BC_NBITS;l++){
+	for(m=0; m<BC_WIDTH; m++){
+	      if(barcode[m][l]==0){
+		  printf("%c",' ');
+	      } else printf("%c",'#');
+	}
+	printf("\n");
+      }  
+    return 0;
 }
 
-int bc_print(int** barcode){
-  int m,l;  
-  for(l=0;l<BC_NBITS;l++){
-    for(m=0; m<BC_WIDTH; m++){
-      if(barcode[m][l]==0){
-	printf(' ');
-      }
-      else{
-	printf('#');
-      }
-      printf('\n');
-      }
-    }
-}
