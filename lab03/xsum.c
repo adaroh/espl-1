1cef58e7384c695252ea1181c66aa1d691226dfd
Yariv Zur <yarivz@post.bgu.ac.il>
2012-11-06 17:49:35 +0200

    xsum

--- lab03/xsum.c
+++ lab03/xsum.c
@@ -1,55 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 int main(int argc, char** argv){
   
   int FLAG=0;
   char* fileName;
-  
-  char opt = getopt(argc, argv, "hx:") {
+  int opt;
+  if((opt = getopt(argc, argv,"-hx:"))!=-1){
     switch (opt) {
+      case 1:
+	fileName = optarg;
+	break;
       case 'h':
-	printf("\nOPTIONS\n\t-h  print a summary of options and exit\n\t-x  print the checksum as a hexadecimal rather than decimal number.");
+	printf("OPTIONS\n\t-h  print a summary of options and exit\n\t-x  print the checksum as a hexadecimal rather than decimal number.\n");
 	exit(0);
       case 'x':
 	FLAG=1;
-        break;
+	fileName = optarg;
+	break;
       default:
 	fprintf(stderr,"Usage: %s [-h] [-x] FILE\n",argv[0]);
-	exit(EXIT_FAILURE);
+	exit(1);
     }
-    char* fileName = optarg;
+  } else {
+    printf("Please provide a filename argument\n");
+    return 0;
   }
+
   
   unsigned int word;
   unsigned int checksum;
-  FILE fp = fopen(filename, "r");
-  if(fp==NULL){
-    printf("The file name provided is not valid");
-    return 1;
+  FILE *fp = fopen(fileName, "r");
+  if(fp==0){
+    printf("The filename argument you provided is not valid\n");
+    return 0;
   }
   fread(&checksum, sizeof(checksum), 1, fp);
   while (fread(&word, sizeof(word), 1, fp)){
     checksum^=word;
   }
   if (FLAG==1){
-    
+    printf("0x%X\n",checksum);
+  } else {
+    printf("%d\n",checksum);
+  }
+  fclose(fp);
   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    	fileName = optarg;
-	fileName = optarg;
-fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-                           argv[0]);
-                   exit(EXIT_FAILURE);
-fprintffprintffprintf
+  return 0;
+}
