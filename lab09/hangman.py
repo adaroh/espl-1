a5aed5d62ea3746b49a217234264923c94d547de
Yariv Zur <yarivz@post.bgu.ac.il>
2013-01-09 15:05:11 +0200

    lab09

--- lab09/hangman.py
+++ lab09/hangman.py
@@ -1,28 +1,30 @@
 import random
-word = list(random.choice(list(open('long-words.txt'))))
+word = (random.choice(list(open('long-words.txt'))))
 try: #attempt to get max nomber of tries from cli argument, default to 6
   limit = sys.argv[1]
 except:
   Exception
   limit=6
-mistakes=0; matches=0; match=False; finished=False; guessed = ['_'*len(word)]
+length=len(word)-1
+mistakes=0; matches=0; match=False; finished=False; guessed = ["_ "]*length; miss=[]
 while mistakes<limit and not finished:
   guess = str(raw_input('Guess a letter: '))
   if guess == 'quit':
-    exit
-  for i in range(len(word)-1):
+    break
+  for i in range(length):
     if guess == word[i]:
       guessed[i] = guess
       match = True
       matches = matches+1
   if not match:
+    miss.append(guess)
     mistakes = mistakes+1
-    print "guessed: ",guessed
-    print "Steps from gallows: ",limit-mistakes, "Word so far: ",guessed
+    print "Steps from gallows: ",limit-mistakes, "Word so far: ",''.join(guessed)
+    print "Missed: ",miss
   else:
-    print guessed
+    print ''.join(guessed)
     match = False
-  if matches == len(word):
+  if matches == (length):
     finished = True
 if mistakes == limit:
   print "** Uh oh: you've run out of steps. You're on the platform.. and <SNAP!>"
